ecb7a752dca8cd43a4af5210ca70a82d5e23a1f8
Rahul Avadhoot <rahulavd@uw.edu>
2017-05-10 17:03:10 -0700

    Updated setup.py to include package data

--- setup.py
+++ setup.py
@@ -51,5 +51,6 @@ setup(name=NAME,
       version=1.0,
       packages=find_packages(),
       install_requires=REQUIRES,
-      requires=REQUIRES
+      requires=REQUIRES,
+      include_package_data=TRUE
 )
